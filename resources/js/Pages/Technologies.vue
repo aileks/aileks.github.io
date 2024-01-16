4e71e705e0db920831454ed6346033eded09db05
aileks <88669313+aileks@users.noreply.github.com>
2024-01-16 10:35:01 -0500

    Minor design fixes

--- resources/js/Pages/Technologies.vue
+++ resources/js/Pages/Technologies.vue
@@ -6,18 +6,16 @@ import Layout from "@/Shared/Layout.vue";
   <Head title="Technologies"/>
 
   <Layout>
-    <div class="flex justify-center items-center h-screen ">
-      <p class="text-xl mx-auto px-4 sm:px-6 lg:px-8 max-w-prose text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Vitae elementum curabitur vitae nunc. Fringilla est ullamcorper eget nulla facilisi etiam
-        dignissim. Et netus et malesuada fames ac turpis egestas integer. Vitae nunc sed velit dignissim sodales ut.
-        Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Rutrum tellus pellentesque eu
-        tincidunt tortor aliquam nulla facilisi. Tempus imperdiet nulla malesuada pellentesque elit eget. Morbi
-        tristique senectus et netus et malesuada fames ac. Nisi scelerisque eu ultrices vitae auctor eu augue ut. Odio
-        facilisis mauris sit amet massa vitae tortor condimentum. Iaculis at erat pellentesque adipiscing commodo elit
-        at. Pharetra et ultrices neque ornare aenean.
-      </p>
-    </div>
+    <p class="text-xl mx-auto px-4 sm:px-6 lg:px-8 max-w-prose text-justify">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Vitae elementum curabitur vitae nunc. Fringilla est ullamcorper eget nulla facilisi etiam
+      dignissim. Et netus et malesuada fames ac turpis egestas integer. Vitae nunc sed velit dignissim sodales ut.
+      Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Rutrum tellus pellentesque eu
+      tincidunt tortor aliquam nulla facilisi. Tempus imperdiet nulla malesuada pellentesque elit eget. Morbi
+      tristique senectus et netus et malesuada fames ac. Nisi scelerisque eu ultrices vitae auctor eu augue ut. Odio
+      facilisis mauris sit amet massa vitae tortor condimentum. Iaculis at erat pellentesque adipiscing commodo elit
+      at. Pharetra et ultrices neque ornare aenean.
+    </p>
   </Layout>
 </template>
 
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -10,18 +10,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="layout">
-    <Nav/>
-    <Transition
-        enter-active-class="transition-all duration-1000 ease-out"
-        enter-from-class="-translate-y-20 opacity-0"
-        enter-to-class="transition-opacity duration-1000"
-        leave-active-class="transition-all duration-1000 cubic-bezier(1, 0, 0, 1)"
-        leave-to-class="-translate-y-20 opacity-0"
-    >
-      <div v-if="animate">
-        <slot/>
-      </div>
-    </Transition>
-  </div>
+  <Nav/>
+  <Transition
+      enter-active-class="transition-all ease-in-out duration-500"
+      enter-from-class="-translate-y-20 opacity-0"
+      enter-to-class="translate-y-0 opacity-100"
+  >
+    <div v-if="animate" class="flex items-center min-h-screen">
+      <slot/>
+    </div>
+  </Transition>
 </template>
