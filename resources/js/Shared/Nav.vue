5238d89fa90c4a76e163b4c30d782c7c0e374e5f
aileks <88669313+aileks@users.noreply.github.com>
2024-01-16 12:32:45 -0500

    Change Link to anchor tag

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -12,9 +12,9 @@ import NavLink from '@/Shared/NavLink.vue'
         <NavLink :active="$page.component === 'Resume'" href="/resume">Resume</NavLink>
       </section>
       <section>
-        <Link href="https://github.com/aileks">
+        <a href="https://github.com/aileks">
           <img alt="Github" class="max-w-6 mt-1" src="../../../public/images/github-mark-white.png"/>
-        </Link>
+        </a>
       </section>
     </header>
   </section>
