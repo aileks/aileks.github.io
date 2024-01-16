3782ec3bba10669136e8863d7a8e4304bd359030
aileks <88669313+aileks@users.noreply.github.com>
2024-01-16 11:34:24 -0500

    Technology SVGs added

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -6,16 +6,9 @@ import Layout from "@/Shared/Layout.vue";
   <Head title="Home"/>
 
   <Layout>
-    <p class="text-xl mx-auto px-4 sm:px-6 lg:px-8 max-w-prose text-justify">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Vitae elementum curabitur vitae nunc. Fringilla est ullamcorper eget nulla facilisi etiam
-      dignissim. Et netus et malesuada fames ac turpis egestas integer. Vitae nunc sed velit dignissim sodales ut.
-      Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Rutrum tellus pellentesque eu
-      tincidunt tortor aliquam nulla facilisi. Tempus imperdiet nulla malesuada pellentesque elit eget. Morbi
-      tristique senectus et netus et malesuada fames ac. Nisi scelerisque eu ultrices vitae auctor eu augue ut. Odio
-      facilisis mauris sit amet massa vitae tortor condimentum. Iaculis at erat pellentesque adipiscing commodo elit
-      at. Pharetra et ultrices neque ornare aenean.
-    </p>
+    <h1 class="text-2xl text-red-600 mx-auto px-4 sm:px-6 lg:px-8 max-w-prose text-justify">
+      &#9888; UNDER CONSTRUCTION &#9888;
+    </h1>
   </Layout>
 </template>
 
