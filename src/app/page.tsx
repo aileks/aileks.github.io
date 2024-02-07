afd188e55e0e8aa7d98a382813492d25ae6e1baf
aileks <88669313+aileks@users.noreply.github.com>
2024-02-07 14:18:48 -0500

    ready for deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 'use client'
+
+import {useEffect} from "react";
 import Image from 'next/image';
 import Link from 'next/link';
 import FollowCursor from './components/followCursor';
 import AnimatedLink from './components/animated-link';
+import {motion, useAnimation} from "framer-motion";
 
 export default function Home() {
+  const headerControls = useAnimation()
+  const aboutControls = useAnimation()
+  const projectsControls = useAnimation()
+  const footerControls = useAnimation()
+
+  useEffect(() => {
+    const sequence = async () => {
+      await headerControls.start({y: 0, opacity: 1, transition: {duration: 0.7}});
+      await Promise.all([
+        aboutControls.start({x: 0, opacity: 1, transition: {duration: 0.7}}),
+        projectsControls.start({x: 0, opacity: 1, transition: {duration: 0.7}})
+      ]);
+      await footerControls.start({y: 0, opacity: 1, transition: {duration: 0.7}});
+    };
+
+    sequence();
+  }, []);
   return (
     <div className="flex flex-col min-h-screen">
       <Image
@@ -14,7 +34,11 @@ export default function Home() {
         height={100}
         className="absolute top-0 left-0 right-0 w-full z-[-1] opacity-5 faint-image"
       />
-      <header className="mb-8">
+      <motion.header
+        initial={{y: '-10', opacity: 0}}
+        animate={headerControls}
+        className="mb-8"
+      >
         <h1 className="text-5xl font-bold flex flex-col mt-12">
             <span className="bg-[linear-gradient(to_right,theme(colors.rose.400),theme(colors.rose.200),theme(colors.red.400),theme(colors.purple.400),theme(colors.fuchsia.400),theme(colors.rose.200),theme(colors.rose.400))] bg-[length:200%_auto] bg-clip-text text-transparent animate-gradient">
               Web Development
@@ -22,10 +46,14 @@ export default function Home() {
 
           <span className="text-4xl font-bold italic">with Passion</span>
         </h1>
-      </header>
+      </motion.header>
 
-      <div className="about-container self-end max-w-md mb-8 mt-2">
-        <h2 className="text-3xl font-bold mb-2 italic">Who the h*ck am I?</h2>
+      <motion.div
+        initial={{x: '10', opacity: 0}}
+        animate={aboutControls}
+        className="about-container self-end max-w-md mb-8 mt-2"
+      >
+        <h2 className="text-3xl font-bold mb-2">Who the h*ck am I?</h2>
         <p className="text-lg text-left">
           My name is Aaliyah and I build stuff sometimes. I'm a Junior at ASU
           pursuing a B.S. in Software Engineering while also learning things
@@ -33,9 +61,13 @@ export default function Home() {
           this passion and curiosity that drives me. My specialties are PHP
           (including Laravel) and JavaScript (including Vue3 + Next.js).
         </p>
-      </div>
+      </motion.div>
 
-      <div className="projects-container w-96 mb-8 mt-2">
+      <motion.div
+        initial={{x: '-10', opacity: 0}}
+        animate={projectsControls}
+        className="projects-container w-96 mb-8 mt-2"
+      >
         <h2 className="text-3xl mb-2 font-bold">Projects</h2>
 
         <ul className="space-y-4">
@@ -76,9 +108,12 @@ export default function Home() {
             </p>
           </li>
         </ul>
-      </div>
+      </motion.div>
 
-      <footer className="flex mt-auto mb-1 self-center">
+      <motion.footer
+        initial={{y: '-5', opacity: 0}}
+        animate={footerControls}
+        className="flex mt-auto sm:pb-3 pb-1 lg:pb-1 md:pb-1 xl:pb-1 self-center">
         <span className="text-sm italic">Made with 💪 by Aaliyah</span>
         <span className="flex">
           <Link href="https://github.com/aileks">
@@ -101,7 +136,7 @@ export default function Home() {
             />
           </Link>
         </span>
-      </footer>
+      </motion.footer>
 
       <FollowCursor/>
     </div>
